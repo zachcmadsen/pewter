ad31e00851b5e852858f597da58c54dff45bcbae
Zach Madsen <zach@zachmadsen.com>
2024-04-15 17:42:13 -0700

    Add basic CLI argument parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,133 +1,158 @@
-use std::ops::Range;
+mod save;
 
-use anyhow::{bail, Result};
+use std::fs;
 
-/// The size of a game save in bytes.
-const SAVE_SIZE: usize = 131072;
-/// The size of a save block in bytes.
-const BLOCK_SIZE: usize = 57344;
-/// The size of a block section in bytes.
-const SECTION_SIZE: usize = 4096;
+use crate::save::{Gender, Save};
+use anyhow::Result;
+use winsafe::{
+    co, gui, prelude::*, CoCreateInstance, CoInitializeEx, IFileOpenDialog,
+    MenuItem, ACCEL, HACCEL, HMENU,
+};
 
-const SECTION_ID_RANGE: Range<usize> = 0x0FF4..(0x0FF4 + 2);
-const SAVE_INDEX_RANGE: Range<usize> = 0x0FFC..(0x0FFC + 4);
+// The values for command IDs are arbitrary.
+const FILE_OPEN_COMMAND_ID: u16 = 0;
 
-const TRAINER_INFO_SECTION_ID: u16 = 0;
-// const TEAM_SECTION_ID: u16 = 1;
-// const GAME_STATE_SECTION_ID: u16 = 2;
-// const MISC_SECTION_ID: u16 = 3;
-// const RIVAL_INFO_SECTION_ID: u16 = 4;
-// const PC_BUFFER_A_SECTION_ID: u16 = 5;
-// const PC_BUFFER_B_SECTION_ID: u16 = 6;
-// const PC_BUFFER_C_SECTION_ID: u16 = 7;
-// const PC_BUFFER_D_SECTION_ID: u16 = 8;
-// const PC_BUFFER_E_SECTION_ID: u16 = 9;
-// const PC_BUFFER_F_SECTION_ID: u16 = 10;
-// const PC_BUFFER_G_SECTION_ID: u16 = 11;
-// const PC_BUFFER_H_SECTION_ID: u16 = 12;
-// const PC_BUFFER_I_SECTION_ID: u16 = 13;
-
-/// The offset of the player gender value in the trainer info section.
-const PLAYER_GENDER_OFFSET: usize = 0x0008;
-
-/// The player gender.
-#[derive(Debug)]
-pub enum Gender {
-    Boy,
-    Girl,
+struct EditorWindow {
+    window: gui::WindowControl,
+    gender_radio_group: gui::RadioGroup,
 }
 
-/// A wrapper for a save file.
-pub struct Save {
-    buf: Box<[u8]>,
-    block_offset: usize,
-    section_offsets: [usize; 14],
-}
+impl EditorWindow {
+    fn new(parent: &gui::WindowMain) -> EditorWindow {
+        let mut options: gui::WindowControlOpts = Default::default();
+        // Match the background color of the parent window.
+        options.class_bg_brush = gui::WindowMainOpts::default().class_bg_brush;
+        options.style = options.style & !co::WS::VISIBLE;
+        // TODO: The size of EditorWindow needs to say in sync with the main window. Listen
+        // on an event for window size changes?
+        options.size = (400, 200);
+        let window = gui::WindowControl::new(parent, options);
 
-impl Save {
-    pub fn new(buf: Vec<u8>) -> Result<Save> {
-        if buf.len() < SAVE_SIZE {
-            bail!("the game save is too small");
+        let _ = gui::Label::new(
+            &window,
+            gui::LabelOpts {
+                text: "Name:".to_owned(),
+                position: (20, 23),
+                ..Default::default()
+            },
+        );
+
+        let _ = gui::Edit::new(
+            &window,
+            gui::EditOpts {
+                position: (75, 20),
+                width: 100,
+                ..Default::default()
+            },
+        );
+
+        let _ = gui::Label::new(
+            &window,
+            gui::LabelOpts {
+                text: "Gender:".to_owned(),
+                position: (20, 60),
+                ..Default::default()
+            },
+        );
+
+        let gender_radio_group = gui::RadioGroup::new(
+            &window,
+            &[
+                gui::RadioButtonOpts {
+                    text: "Boy".to_owned(),
+                    position: (75, 60),
+                    ..Default::default()
+                },
+                gui::RadioButtonOpts {
+                    text: "Girl".to_owned(),
+                    position: (125, 60),
+                    ..Default::default()
+                },
+            ],
+        );
+
+        EditorWindow { window, gender_radio_group }
+    }
+
+    fn load(&self, buf: Vec<u8>) {
+        let save = Save::new(buf).unwrap();
+
+        self.init_gender(&save);
+
+        self.window.hwnd().ShowWindow(co::SW::SHOW);
+    }
+
+    fn init_gender(&self, save: &Save) {
+        // Clear selected buttons, if any.
+        for radio_button in self.gender_radio_group.iter() {
+            radio_button.select(false);
         }
 
-        let save_blocks = &buf[..(BLOCK_SIZE * 2)];
-        let (block_a, block_b) = save_blocks.split_at(BLOCK_SIZE as usize);
-
-        let save_index_a = read_u32(&block_a[SAVE_INDEX_RANGE]);
-        let save_index_b = read_u32(&block_b[SAVE_INDEX_RANGE]);
-
-        // TODO: Try the other block if the most recent block is invalid.
-        let (block_offset, section_offsets) = if save_index_a > save_index_b {
-            (0, validate_sections(block_a)?)
-        } else {
-            (BLOCK_SIZE, validate_sections(block_b)?)
+        let gender = save.player_gender();
+        let gender_radio_index = match gender {
+            Gender::Boy => 0,
+            Gender::Girl => 1,
         };
-
-        Ok(Save { buf: buf.into_boxed_slice(), block_offset, section_offsets })
-    }
-
-    pub fn player_gender(&self) -> Gender {
-        let section = self.section(TRAINER_INFO_SECTION_ID);
-        match section[PLAYER_GENDER_OFFSET] {
-            0x00 => Gender::Boy,
-            0x01 => Gender::Girl,
-            _ => panic!("invalid player gender value"),
-        }
-    }
-
-    fn section(&self, id: u16) -> &[u8] {
-        let start = self.block_offset;
-        let end = start + BLOCK_SIZE;
-        let block = &self.buf[start..end];
-
-        let section_start = self.section_offsets[id as usize];
-        let section_end = section_start + SECTION_SIZE;
-        &block[section_start..section_end]
+        self.gender_radio_group[gender_radio_index].select(true);
     }
 }
 
-fn validate_sections(block: &[u8]) -> Result<[usize; 14]> {
-    debug_assert!(block.len() == BLOCK_SIZE);
+pub fn run() -> Result<()> {
+    let file_submenu = HMENU::CreatePopupMenu()?;
+    file_submenu.append_item(&[MenuItem::Entry(
+        FILE_OPEN_COMMAND_ID,
+        "Open\tCtrl+O",
+    )])?;
 
-    let mut seen_section_ids = [false; 14];
-    let mut section_offsets = [0; 14];
+    let menu = HMENU::CreateMenu()?;
+    menu.append_item(&[MenuItem::Submenu(&file_submenu, "File")])?;
 
-    let save_index = read_u32(&block[SAVE_INDEX_RANGE]);
+    let accel_table = HACCEL::CreateAcceleratorTable(&mut [ACCEL {
+        fVirt: co::ACCELF::VIRTKEY | co::ACCELF::CONTROL,
+        cmd: FILE_OPEN_COMMAND_ID,
+        key: co::VK::CHAR_O,
+    }])?;
 
-    for (i, section) in block.chunks_exact(SECTION_SIZE).enumerate() {
-        if read_u32(&block[SAVE_INDEX_RANGE]) != save_index {
-            bail!("not all sections have the same save index");
+    let main_window = gui::WindowMain::new(gui::WindowMainOpts {
+        title: "Firered Save Editor".to_owned(),
+        size: (400, 200),
+        menu,
+        accel_table: Some(accel_table),
+        ..Default::default()
+    });
+
+    let editor_window = EditorWindow::new(&main_window);
+
+    main_window.on().wm_command_accel_menu(FILE_OPEN_COMMAND_ID, {
+        let main_window = main_window.clone();
+        move || {
+            let file_dialog = CoCreateInstance::<IFileOpenDialog>(
+                &co::CLSID::FileOpenDialog,
+                None,
+                co::CLSCTX::INPROC_SERVER,
+            )?;
+            // Only accept file system items.
+            file_dialog.SetOptions(
+                file_dialog.GetOptions()? | co::FOS::FORCEFILESYSTEM,
+            )?;
+
+            if file_dialog.Show(main_window.hwnd())? {
+                let item = file_dialog.GetResult()?;
+                let file_path = item.GetDisplayName(co::SIGDN::FILESYSPATH)?;
+
+                let buf = fs::read(file_path)?;
+                editor_window.load(buf);
+            }
+
+            Ok(())
         }
+    });
 
-        let section_id = read_u16(&section[SECTION_ID_RANGE]);
-        if !(0..=13).contains(&section_id) {
-            bail!("invalid section ID");
-        }
+    let _com = CoInitializeEx(
+        co::COINIT::APARTMENTTHREADED | co::COINIT::DISABLE_OLE1DDE,
+    )?;
 
-        if seen_section_ids[section_id as usize] {
-            bail!("repeat section ID");
-        } else {
-            seen_section_ids[section_id as usize] = true;
-        }
+    main_window.run_main(None).unwrap();
 
-        section_offsets[section_id as usize] = i * SECTION_SIZE;
-    }
-
-    // I think this condition is always true after the loop, but I'll keep it
-    // in anyways.
-    if !seen_section_ids.iter().all(|b| *b) {
-        bail!("every section didn't appear once");
-    }
-
-    Ok(section_offsets)
-}
-
-/// Reads an unsigned 16 bit integer from `buf`.
-fn read_u16(buf: &[u8]) -> u16 {
-    u16::from_le_bytes(buf[..2].try_into().unwrap())
-}
-
-/// Reads an unsigned 32 bit integer from `buf`.
-fn read_u32(buf: &[u8]) -> u32 {
-    u32::from_le_bytes(buf[..4].try_into().unwrap())
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,156 +1,67 @@
-use std::fs;
+const USAGE: &str = "\
+Usage: fled [OPTIONS] FILE
 
-use anyhow::Result;
-use firered_save_editor::{Gender, Save};
-use winsafe::{
-    co, gui, prelude::*, CoCreateInstance, CoInitializeEx, IFileOpenDialog,
-    MenuItem, ACCEL, HACCEL, HMENU,
-};
+Options:
+    -h, --help       Print help info and exit
+    -v, --version    Print version info and exit";
 
-// The values for command IDs are arbitrary.
-const FILE_OPEN_COMMAND_ID: u16 = 0;
-
-struct EditorWindow {
-    window: gui::WindowControl,
-    gender_radio_group: gui::RadioGroup,
+struct Args {
+    help: bool,
+    version: bool,
+    filename: Option<String>,
 }
 
-impl EditorWindow {
-    fn new(parent: &gui::WindowMain) -> EditorWindow {
-        let mut options: gui::WindowControlOpts = Default::default();
-        // Match the background color of the parent window.
-        options.class_bg_brush = gui::WindowMainOpts::default().class_bg_brush;
-        options.style = options.style & !co::WS::VISIBLE;
-        // TODO: The size of EditorWindow needs to say in sync with the main window. Listen
-        // on an event for window size changes?
-        options.size = (400, 200);
-        let window = gui::WindowControl::new(parent, options);
+fn parse_args() -> Args {
+    let mut args = Args { help: false, version: false, filename: None };
 
-        let _ = gui::Label::new(
-            &window,
-            gui::LabelOpts {
-                text: "Name:".to_owned(),
-                position: (20, 23),
-                ..Default::default()
-            },
-        );
+    let mut pargs = pico_args::Arguments::from_env();
 
-        let _ = gui::Edit::new(
-            &window,
-            gui::EditOpts {
-                position: (75, 20),
-                width: 100,
-                ..Default::default()
-            },
-        );
-
-        let _ = gui::Label::new(
-            &window,
-            gui::LabelOpts {
-                text: "Gender:".to_owned(),
-                position: (20, 60),
-                ..Default::default()
-            },
-        );
-
-        let gender_radio_group = gui::RadioGroup::new(
-            &window,
-            &[
-                gui::RadioButtonOpts {
-                    text: "Boy".to_owned(),
-                    position: (75, 60),
-                    ..Default::default()
-                },
-                gui::RadioButtonOpts {
-                    text: "Girl".to_owned(),
-                    position: (125, 60),
-                    ..Default::default()
-                },
-            ],
-        );
-
-        EditorWindow { window, gender_radio_group }
+    if pargs.contains(["-h", "--help"]) {
+        args.help = true;
+        return args;
     }
 
-    fn load(&self, buf: Vec<u8>) {
-        let save = Save::new(buf).unwrap();
-
-        self.init_gender(&save);
-
-        self.window.hwnd().ShowWindow(co::SW::SHOW);
+    if pargs.contains(["-v", "--version"]) {
+        args.version = true;
+        return args;
     }
 
-    fn init_gender(&self, save: &Save) {
-        // Clear selected buttons, if any.
-        for radio_button in self.gender_radio_group.iter() {
-            radio_button.select(false);
+    // TODO: Add debug and warn logging.
+    args.filename = match pargs.opt_free_from_str() {
+        Ok(Some(filename)) => Some(filename),
+        _ => None,
+    };
+
+    let remaining = pargs.finish();
+    if !remaining.is_empty() {
+        eprint!("warn: ignoring extra arguments: ");
+        for arg in remaining {
+            eprint!("{} ", arg.to_string_lossy());
         }
-
-        let gender = save.player_gender();
-        let gender_radio_index = match gender {
-            Gender::Boy => 0,
-            Gender::Girl => 1,
-        };
-        self.gender_radio_group[gender_radio_index].select(true);
     }
+
+    args
 }
 
-fn main() -> Result<()> {
-    let file_submenu = HMENU::CreatePopupMenu()?;
-    file_submenu.append_item(&[MenuItem::Entry(
-        FILE_OPEN_COMMAND_ID,
-        "Open\tCtrl+O",
-    )])?;
+fn main() {
+    let args = parse_args();
 
-    let menu = HMENU::CreateMenu()?;
-    menu.append_item(&[MenuItem::Submenu(&file_submenu, "File")])?;
+    if args.help {
+        println!("{}", USAGE);
+        return;
+    }
 
-    let accel_table = HACCEL::CreateAcceleratorTable(&mut [ACCEL {
-        fVirt: co::ACCELF::VIRTKEY | co::ACCELF::CONTROL,
-        cmd: FILE_OPEN_COMMAND_ID,
-        key: co::VK::CHAR_O,
-    }])?;
+    if args.version {
+        println!(
+            "fled {} ({})",
+            env!("CARGO_PKG_VERSION"),
+            env!("COMMIT_HASH")
+        );
+        return;
+    }
 
-    let main_window = gui::WindowMain::new(gui::WindowMainOpts {
-        title: "Firered Save Editor".to_owned(),
-        size: (400, 200),
-        menu,
-        accel_table: Some(accel_table),
-        ..Default::default()
-    });
-
-    let editor_window = EditorWindow::new(&main_window);
-
-    main_window.on().wm_command_accel_menu(FILE_OPEN_COMMAND_ID, {
-        let main_window = main_window.clone();
-        move || {
-            let file_dialog = CoCreateInstance::<IFileOpenDialog>(
-                &co::CLSID::FileOpenDialog,
-                None,
-                co::CLSCTX::INPROC_SERVER,
-            )?;
-            // Only accept file system items.
-            file_dialog.SetOptions(
-                file_dialog.GetOptions()? | co::FOS::FORCEFILESYSTEM,
-            )?;
-
-            if file_dialog.Show(main_window.hwnd())? {
-                let item = file_dialog.GetResult()?;
-                let file_path = item.GetDisplayName(co::SIGDN::FILESYSPATH)?;
-
-                let buf = fs::read(file_path)?;
-                editor_window.load(buf);
-            }
-
-            Ok(())
-        }
-    });
-
-    let _com = CoInitializeEx(
-        co::COINIT::APARTMENTTHREADED | co::COINIT::DISABLE_OLE1DDE,
-    )?;
-
-    main_window.run_main(None).unwrap();
-
-    Ok(())
+    if let Err(x) = firered_save_editor::run() {
+        eprintln!("error: {}", x.to_string());
+        std::process::exit(1);
+    }
 }
