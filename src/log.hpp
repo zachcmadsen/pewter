bac0f761b0ef9675b54e08e449399dca6ce52a49
Zach Madsen <zach@zachmadsen.com>
2024-06-06 18:27:52 -0700

    Add a doc comment

--- src/log.hpp
+++ src/log.hpp
@@ -7,6 +7,7 @@
 
 namespace pewter {
 
+/// Logs a message to `stderr`.
 template <typename... T> void log(fmt::format_string<T...> fmt, T &&...args) {
     auto msg = fmt::format(fmt, std::forward<T>(args)...);
     fmt::print(stderr, "{}\n", msg);
