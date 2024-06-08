9a3d4adb6d41a2ff556ac77be3436630ef8da14e
Zach Madsen <zach@zachmadsen.com>
2024-06-07 17:26:40 -0700

    Fix warnings in util.hpp

--- src/util.hpp
+++ src/util.hpp
@@ -9,16 +9,17 @@
 namespace pewter {
 
 /// Reads a 16-bit unsigned integer from `bytes`.
-inline uint16_t readU16(std::span<const uint8_t, 2> bytes) {
-    return bytes[0] | bytes[1] << 8;
+inline std::uint16_t readU16(std::span<const std::uint8_t, 2> bytes) {
+    return static_cast<std::uint16_t>(bytes[0] | bytes[1] << 8);
 }
 
 /// Reads a 32-bit unsigned integer from `bytes`.
-inline uint32_t readU32(std::span<const uint8_t, 4> bytes) {
-    return bytes[0] | bytes[1] << 8 | bytes[2] << 16 | bytes[3] << 24;
+inline std::uint32_t readU32(std::span<const std::uint8_t, 4> bytes) {
+    return static_cast<std::uint32_t>(bytes[0] | bytes[1] << 8 |
+                                      bytes[2] << 16 | bytes[3] << 24);
 }
 
 /// Reads the contents of a file into a vector of bytes.
-std::optional<std::vector<uint8_t>> readFile(std::string_view filename);
+std::optional<std::vector<std::uint8_t>> readFile(std::string_view filename);
 
 }
